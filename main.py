3c72c3ad6a4808f225280fe2bc02c07602d6aba5
dwctsai <61249617+dwctsai@users.noreply.github.com>
2021-05-26 22:58:34 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,67 @@
+from turtle import Screen
+from snake import Snake  # from snake file import Snake class
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+
+# Setup Screen
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)  # turn off tracer
+
+# Create a Snake
+snake = Snake()
+
+# Create a Food
+food = Food()
+
+# Create a Scoreboard
+scoreboard = Scoreboard()
+
+# Listen for keystrokes
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+# Game loop
+game_is_on = True
+while game_is_on:
+    screen.update()  # update the screen
+    time.sleep(0.1)  # add a delay
+
+    snake.move()
+
+    # Detect collision with food
+    # Snake segments are 20x20.  Foods are 10x10.
+    if snake.head.distance(food) < 15:
+        print("nom nom nom")
+        # Collision has occurred between Snake head segment and a food.
+        # Move the food to a new location.
+        food.refresh()
+        # Extend snake length by 1 segment
+        snake.extend()
+        # Update scoreboard
+        scoreboard.increase_score()
+
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.reset()
+        snake.reset()
+
+    # Detect collision with tail
+    # Skip the head itself
+    # snake.segments[1:] returns a slice of the list of segments excluding the head.
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
+
+
+# Close window if mouse click on screen
+screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,71 @@
+from turtle import Turtle
+
+
+# Constants
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        # Create a snake body
+        # Each segment is a Turtle with the shape of a square and size 20x20.
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    def extend(self):
+        # Get the position of the last segment.
+        # Then add a segment to that position.
+        # Remember in Python for a list [1, 2, 3], list[-1] is 3.
+        self.add_segment(self.segments[-1].position())
+
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)  # move the segment off-screen
+        self.segments.clear()  # clear the list
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def move(self):
+        # Move last segment to second-to-last segment, backwards all the way to the head
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        # Move the head segment
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        # The snake can't be allowed to move backwards from its current heading
+        if self.head.heading() != DOWN:
+            # Set the head's heading
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
